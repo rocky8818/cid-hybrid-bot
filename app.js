d0776e97ceca3d82afc8d49bf07cc4e76783bf8c
rocky8818 <rickyglz.1318@gmail.com>
2024-08-06 16:46:54 -0600

    🚧 - added the report flow and the test report flow

--- app.js
+++ app.js
@@ -18,6 +18,36 @@ const intro = fs.readFileSync(introPath, "utf8")
 const pathConsultas = path.join(__dirname, "mensajes", "promptConsultas.md")
 const promptConsultas = fs.readFileSync(pathConsultas, "utf8")
 
+const flowNewReport = addKeyword('Enviar Reporte')
+    .addAnswer((ctx, [gotoFlow]) => {
+        return gotoFlow(flowReporte)
+    })
+
+const testReport = addKeyword('sprReport')
+.addAction(async(ctx) =>{
+    const report = {
+        empresa: 'cid',
+        telefono: '8272366329',
+        contacto: 'ricardo',
+        correo: 'devœcid',
+        descripcion: 'test de report wa',
+        tema: 'Correos',
+        impacto: 'myState.impacto',
+        so: 'mac',
+        dispositivo: 'desktop',
+        prioridad: 'Alta',
+        adicional: 'myState.adicional',
+        from: 'Whatsapp',
+        estado: 'Pausa'
+    };
+    console.log(report);
+
+    try {
+        const response = await axios.post(process.env.TICKETS_API, report);
+    } catch (error) {
+        console.error('Error al enviar el reporte:', error);
+    }
+})
 
 const flowConsultas = addKeyword(EVENTS.ACTION)
     .addAnswer('Este es el flow consultas')
@@ -28,48 +58,56 @@ const flowConsultas = addKeyword(EVENTS.ACTION)
         await ctxFn.flowDynamic(answer.content)
     })
 
-    const flowIntro = addKeyword(EVENTS.ACTION)
-        .addAnswer(intro)
+const flowIntro = addKeyword(EVENTS.ACTION)
+    .addAnswer(intro)
 
-const flowProductos = addKeyword(EVENTS.ACTION)
-    .addAnswer('Te enviaré los 3 productos que tenemos, cada uno es especial para distintas situaciones', 'Siéntete en confianza de preguntar especificamente sobre el que te interese.',{
+const flowAgencia = addKeyword(EVENTS.ACTION)
+    .addAnswer('En la agencia de cid manejamos servicios como desarrollo web, branding, foto y video, arquitectura e interiorismo entre otros.', {
+        delay: 3000
+    })
+    .addAnswer('Puedes encontrar más información de estos servicios en nuestra página web agencia.cid.mx o puedes preguntar en este chat eligiendo la opción 5 en el menú.', {
         delay: 4000
     })
-    .addAnswer('Nuestro primer bot es Rowena flow, ideal si tienes bien determinado las respuestas que quieres dar.', {
+    .addAnswer('Nos encantaría trabajar contigo!', {
         media: 'https://i.pinimg.com/564x/6c/ae/9b/6cae9b731a26eeefdb3ae01be62f7d9b.jpg',
         delay: 2000
     })
-    .addAnswer('El siguiente es Rowena personal assistant, nuestar opción 100% inteligente', {
-        media: 'https://i.pinimg.com/564x/eb/52/af/eb52af485575ea64d0e6d722622dfc1a.jpg',
-        delay: 2000
+
+const flowEscuela = addKeyword(EVENTS.ACTION)
+    .addAnswer('En el apartado de escuela tenemos cursos donde obtendrás conocimientos sobre las herramientas más utilizadas de diseño. Con cursos como photoshop, ilustrador o autocad.', {
+        delay: 3000
     })
-    .addAnswer('Por último, tenemos a Rowena Hybrid, nuestar opción más completa, una mezcla de los dos bots anteriores.', {
-        media: 'https://i.pinimg.com/736x/54/cc/8f/54cc8fb7ce3a4459a68de390569c496c.jpg',
-        delay: 2000
+    .addAnswer('Si te interesa conocer más sobre la escuela puedes preguntar en la opción cuatro del menú *Que cursos maneja la escuela de cid* ó visita nuestra página web escuela.cid.mx', {
+        delay: 4000
     })
-    .addAnswer('Si tienes alguna duda no dudes en preguntar en la opción 4 del menú', {
+    .addAnswer('Con nuestros cursos nunca dejarás de aprender!', {
+        media: 'https://i.pinimg.com/564x/6c/ae/9b/6cae9b731a26eeefdb3ae01be62f7d9b.jpg',
         delay: 2000
     })
 
 
 const menuFlow = addKeyword(EVENTS.WELCOME).addAnswer(
-    menu,
-    { capture: true },
+    menu, { 
+        capture: true,
+        delay: 3000,
+     },
     async (ctx, { gotoFlow, fallBack, flowDynamic }) => {
-        if (!["1", "2", "3", "4", "0"].includes(ctx.body)) {
+        if (!["1", "2", "3", "4", "5", "0"].includes(ctx.body)) {
             return fallBack(
-                "Respuesta no válida, por favor selecciona una de las opciones."
+                "Respuesta no válida, por favor envía (0, 1, 2, 3, 4 ó 5)."
             );
         }
         switch (ctx.body) {
             case "1":
                 return gotoFlow(flowIntro);
             case "2":
-                return gotoFlow(flowProductos);
+                return gotoFlow(flowAgencia);
             case "3":
-                return gotoFlow(flowReporte);
+                return gotoFlow(flowEscuela);
             case "4":
-                return gotoFlow(flowConsultas);
+                return gotoFlow(flowReporte);
+            case "5":
+                return gotoFlow(flowConsultas)
             case "0":
                 return await flowDynamic(
                     "Saliendo... Puedes volver a acceder a este menú escribiendo '*Menu*'"
@@ -78,129 +116,209 @@ const menuFlow = addKeyword(EVENTS.WELCOME).addAnswer(
     }
 );
 
-    const flowReporte = addKeyword(['reporte'])
-        .addAnswer(
-            ['Hola 👋, sentimos que estés teniendo problemas, pero intentaremos resolverlo.', 'A continuación haremos unas preguntas ❔ para recaudar información de tu problema y atenderlo de la mejor manera.🧐']
-        )
-        .addAnswer(
-            '¿De qué empresa te estás comunicando?🏭',
-            {
-                capture: true,
-            },
-            async (ctx, { state }) => {
-                await state.update({ empresa: ctx.body });
-                await state.update({ telefono: ctx.from });
+const flowReporte = addKeyword(['reporte'])
+    .addAnswer(
+        ['Hola 👋, sentimos que estés teniendo problemas, pero intentaremos resolverlo.',, 'A continuación haremos unas preguntas ❔ para recaudar información de tu problema y atenderlo de la mejor manera.🧐']
+    )
+    .addAnswer(
+        'Si en algún momento deseas salir del flujo ❌ y no enviar el reporte, envía *cancelar*. '
+    )
+    .addAnswer(
+        ['¿De qué empresa te estás comunicando?🏭'],
+        {
+            capture: true,
+            idle: 3000
+        },
+        async (ctx, { state, endFlow, gotoFlow }) => {
+            if(ctx.body == 'Cancelar' || ctx.body == 'cancelar'){
+                return endFlow({body: 'Tu reporte ha sido cancelado ❌'})
             }
-        )
-        .addAnswer(
-            '👷¿Cuál es tu nombre?👷‍♀️',
-            {
-                capture: true,
-            },
-            async (ctx, { state }) => {
-                await state.update({ contacto: ctx.body });
+            if(ctx?.idleFallBack){
+                return gotoFlow
+            }(flujoFinal)
+            await state.update({ empresa: ctx.body });
+            await state.update({ telefono: ctx.from });
+        }
+    )
+    .addAnswer(
+        ['👷¿Cuál es tu nombre?👷‍♀️'],
+        {
+            capture: true,
+            idle: 30000
+        },
+        async (ctx, { state, endFlow, gotoFlow }) => {
+            if(ctx.body == 'Cancelar' || ctx.body == 'cancelar'){
+                return endFlow({body: 'Tu reporte ha sido cancelado ❌'})
             }
-        )
-        .addAnswer(
-            '¿Cuál es tu correo?📭',
-            {
-                capture: true,
-            },
-            async (ctx, { state }) => {
-                await state.update({ correo: ctx.body });
+            if(ctx?.idleFallBack){
+                return gotoFlow
+            }(flujoFinal)
+            await state.update({ contacto: ctx.body });
+        }
+    )
+    .addAnswer(
+        ['¿Cuál es tu correo?📭', , 'Recuerda que si deseas salir del flujo, puedes envíar *cancelar*'],
+        {
+            capture: true,
+            idle: 300000
+        },
+        async (ctx, { state, endFlow, gotoFlow }) => {
+            if(ctx.body == 'Cancelar' || ctx.body == 'cancelar'){
+                return endFlow({body: 'Tu reporte ha sido cancelado ❌'})
             }
-        )
-        .addAnswer(
-            '¿Qué problemas tienes?✍️',
-            {
-                capture: true,
-            },
-            async (ctx, { state }) => {
-                await state.update({ descripcion: ctx.body });
+            if(ctx?.idleFallBack){
+                return gotoFlow
+            }(flujoFinal)
+            await state.update({ correo: ctx.body });
+        }
+    )
+    .addAnswer(
+        '¿Qué problemas tienes?✍️',
+        {
+            capture: true,
+            idle: 300000
+        },
+        async (ctx, { state, endFlow, gotoFlow }) => {
+            if(ctx.body == 'Cancelar' || ctx.body == 'cancelar'){
+                return endFlow({body: 'Tu reporte ha sido cancelado ❌'})
             }
-        )
-        .addAnswer(
-            '¿Nos puedes contar como te está afectando el problema?📈',
-            {
-                capture: true,
-            },
-            async (ctx, { state }) => {
-                await state.update({ impacto: ctx.body });
+            if(ctx?.idleFallBack){
+                return gotoFlow
+            }(flujoFinal)
+            await state.update({ descripcion: ctx.body });
+        }
+    )
+    .addAnswer(
+        ['A cual de estos temas relacionas tu problema? 📚', '', 'Correos, Web, Aplicaciones, Redes'],
+        async (ctx, { fallBack }) => {
+            if (!["Correos", "Web", "Aplicaciones", "Redes", "Cancelar", "cancelar"].includes(ctx.body)) {
+                return fallBack(
+                    "Respuesta no válida, por favor envía (Correos, Web, Aplicaciones, Redes, Cancelar)."
+                );
             }
-        )
-        .addAnswer(
-            '💻¿En qué software estás encontrando el problema el problema? (windows, mac, linux, android, ios, no aplica)',
-            {
-                capture: true,
-            },
-            async (ctx, { state }) => {
-                await state.update({ so: ctx.body });
+        }
+    )
+    .addAnswer(
+        '¿Nos puedes contar como te está afectando el problema?📈',
+        {
+            capture: true,
+            idle: 300000
+        },
+        async (ctx, { state, endFlow, gotoFlow }) => {
+            if(ctx.body == 'Cancelar' || ctx.body == 'cancelar'){
+                return endFlow({body: 'Tu reporte ha sido cancelado ❌'})
             }
-        )
-        .addAnswer(
-            '¿En qué tipo de dispositivo encontró el problema?📱 (Desktop, tablet, celular, no aplica)',
-            {
-                capture: true,
-            },
-            async (ctx, { state }) => {
-                await state.update({ dispositivo: ctx.body });
+            if(ctx?.idleFallBack){
+                return gotoFlow
+            }(flujoFinal)
+            await state.update({ impacto: ctx.body });
+        }
+    )
+    .addAnswer(
+        '💻¿En qué software estás encontrando el problema el problema? (windows, mac, linux, android, ios, no aplica)',
+        {
+            capture: true,
+            idle: 300000
+        },
+        async (ctx, { state, endFlow, gotoFlow }) => {
+            if(ctx.body == 'Cancelar' || ctx.body == 'cancelar'){
+                return endFlow({body: 'Tu reporte ha sido cancelado ❌'})
             }
-        )
-        .addAnswer(
-            '¿Cuál prioridad describiría mejor el problema?🚦 (Baja, media, alta)',
-            {
-                capture: true,
-            },
-            async (ctx, { state }) => {
-                await state.update({ prioridad: ctx.body });
+            if(ctx?.idleFallBack){
+                return gotoFlow
+            }(flujoFinal)
+            await state.update({ so: ctx.body });
+        }
+    )
+    .addAnswer(
+        '¿En qué tipo de dispositivo encontró el problema?📱 (Desktop, tablet, celular, no aplica)',
+        {
+            capture: true,
+            idle: 300000
+        },
+        async (ctx, { state, endFlow, gotoFlow }) => {
+            if(ctx.body == 'Cancelar' || ctx.body == 'cancelar'){
+                return endFlow({body: 'Tu reporte ha sido cancelado ❌'})
             }
-        )
-        .addAnswer(
-            '¿Tiene alguna información adicional que nos ayude a tener un mejor panorama 🌄 del problema?🗣️',
-            {
-                capture: true,
-            },
-            async (ctx, { state }) => {
-                await state.update({ adicional: ctx.body });
-                const myState = state.getMyState();
+            if(ctx?.idleFallBack){
+                return gotoFlow
+            }(flujoFinal)
+            await state.update({ dispositivo: ctx.body });
+        }
+    )
+    .addAnswer(
+        '¿Cuál prioridad describiría mejor el problema?🚦 (Baja, media, alta)',
+        {
+            capture: true,
+            idle: 300000
+        },
+        async (ctx, { state, endFlow, gotoFlow }) => {
+            if(ctx.body == 'Cancelar' || ctx.body == 'cancelar'){
+                return endFlow({body: 'Tu reporte ha sido cancelado ❌'})
+            }
+            if(ctx?.idleFallBack){
+                return gotoFlow
+            }(flujoFinal)
+            await state.update({ prioridad: ctx.body });
+        }
+    )
+    .addAnswer(
+        '¿Tiene alguna información adicional que nos ayude a tener un mejor panorama 🌄 del problema?🗣️',
+        {
+            capture: true,
+            idle: 3000
+        },
+        async (ctx, { state, endFlow, gotoFlow }) => {
+            if(ctx.body == 'Cancelar' || ctx.body == 'cancelar'){
+                return endFlow({body: 'Tu reporte ha sido cancelado ❌'})
+            }
+            if(ctx?.idleFallBack){
+                return gotoFlow
+            }(flujoFinal)
+            await state.update({ adicional: ctx.body });
+            const myState = state.getMyState();
 
-                const report = {
-                    empresa: myState.empresa,
-                    telefono: myState.telefono,
-                    contacto: myState.contacto,
-                    correo: myState.correo,
-                    descripcion: myState.descripcion,
-                    impacto: myState.impacto,
-                    so: myState.so,
-                    dispositivo: myState.dispositivo,
-                    prioridad: myState.prioridad,
-                    adicional: myState.adicional
-                };
-                console.log(report);
-                await state.update({ resp: 'Pronto nos comunicaremos contigo' });
-                
-                try {
-                    const response = await axios.post('http://localhost:4321/api/reportwhatsapp', report);
-                    const ticketId = response.data.id;
-                    await state.update({ resp: `Listo🤖, tu número de ticket es ${ticketId}` });
-                } catch (error) {
-                    console.error('Error al enviar el reporte:', error);
-                    await state.update({ resp: `Listo🤖, tu número de ticket es ${ticketId}` });
-                }
+            const report = {
+                empresa: myState.empresa,
+                telefono: myState.telefono,
+                contacto: myState.contacto,
+                correo: myState.correo,
+                descripcion: myState.descripcion,
+                tema: myState.tema,
+                impacto: myState.impacto,
+                so: myState.so,
+                dispositivo: myState.dispositivo,
+                prioridad: myState.prioridad,
+                adicional: myState.adicional,
+                from: 'Whatsapp',
+                estado: 'Abierto'
+            };
+            console.log(report);
+            await state.update({ resp: 'Pronto nos comunicaremos contigo' });
+
+            try {
+                const response = await axios.post(process.env.TICKETS_API, report);
+                const ticketId = response.data.id;
+                await state.update({ resp: `Listo🤖, tu número de ticket es ${ticketId}` });
+            } catch (error) {
+                console.error('Error al enviar el reporte:', error);
             }
-        )
-        .addAnswer(
-            'Hemos recibido tu reporte!',
-            null,
-            async (ctx, { flowDynamic, state }) => {
-                const resp = state.get('resp');
-                await flowDynamic(`${resp}`);
-            }
-        );
+        }
+    )
+    .addAnswer(
+        'Hemos recibido tu reporte!',
+        null,
+        async (ctx, { flowDynamic, state }) => {
+            const resp = state.get('resp');
+            await flowDynamic(`${resp}`);
+        }
+    );
+
+const flujoFinal = addKeyword(EVENTS.ACTION).addAnswer('No recibimos tu respuesta y se canceló la solicitud.')
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowConsultas, menuFlow, flowReporte])
+    const adapterFlow = createFlow([flowConsultas, menuFlow, flowReporte, flujoFinal, testReport, flowNewReport])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
